e8260d0dabc44c764d94bf13c78a5ca57648780f
PashaDem <dem-pasha@lenta.ru>
2023-05-06 09:13:13 +0300

    init: reinit project structure

--- backend/src/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from .v1 import router_v1
-
-api_router = APIRouter()
-
-api_router.include_router(router_v1, prefix="/v1")
--- backend/src/api/v1/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from .register_user import register_user_router
-
-router_v1 = APIRouter()
-
-router_v1.include_router(register_user_router)
